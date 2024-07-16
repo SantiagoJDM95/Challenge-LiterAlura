fb7895261694542f5ff4168af80a345dd2977214
SantiagoJDM95 <Santiagojdm95@hotmail.com>
2024-07-16 05:15:38 -0600

    Initial commit

---
+++ src/test/java/com/literalura_Challenge/Challenge/LiterAlura/ChallengeLiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literalura_Challenge.Challenge.LiterAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
